196b0815c410efa8270d94bf6103c23820a6512e
Julien Levasseur <julien.levasseur@beemhq.com>
2020-11-30 16:33:39 -0500

    feat(VSCode) Adds support for ubuntu

--- main.tf
+++ main.tf
@@ -1,9 +1,17 @@
+resource "null_resource" "repository" {
+  # See: https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo
+  provisioner "local-exec" {
+    command = "sudo apt install -y extrepo && sudo extrepo enable vscodium"
+    interpreter = var.default_interpreter
+  }
+}
+
 resource "null_resource" "vscode" {
 
-    provisioner "local-exec" {
-        command = "[ ! -d \"/Applications/VSCodium.app\" ] && brew install vscodium"
-        interpreter = var.default_interpreter
-    }
+  provisioner "local-exec" {
+    command = "sudo apt update && sudo apt install -y codium"
+    interpreter = var.default_interpreter
+  }
 }
 
 resource "null_resource" "set_microsoft_marketplace" {
@@ -13,18 +21,15 @@ resource "null_resource" "set_microsoft_marketplace" {
     # See: https://github.com/VSCodium/vscodium/blob/master/DOCS.md#extensions-marketplace
 
     provisioner "local-exec" {
-        # The -i '' is mandatory here because of the BSD sed verison that expect
-        # a suffix. The GNU sed take this suffix as optional.
-        # See: https://stackoverflow.com/questions/16745988/sed-command-with-i-option-in-place-editing-works-fine-on-ubuntu-but-not-mac
         command = "sed -i '' 's,https://open-vsx.org/vscode/gallery,https://marketplace.visualstudio.com/_apis/public/gallery,g' product.json"
         interpreter = var.default_interpreter
-        working_dir = "/Applications/VSCodium.app/Contents/Resources/app"
+        working_dir = "/usr/share/codium/resources/app/"
     }
 
     provisioner "local-exec" {
         command = "sed -i '' 's,https://open-vsx.org/vscode/item,https://marketplace.visualstudio.com/items,g' product.json"
         interpreter = var.default_interpreter
-        working_dir = "/Applications/VSCodium.app/Contents/Resources/app"
+        working_dir = "/usr/share/codium/resources/app/"
     }
 }
 
@@ -32,7 +37,7 @@ resource "null_resource" "vscode_extension" {
     for_each = toset(var.extensions)
 
     provisioner "local-exec" {
-        command = "code --install-extension ${each.key}"
+        command = "codium --install-extension ${each.key}"
         interpreter = var.default_interpreter
     }
 }
